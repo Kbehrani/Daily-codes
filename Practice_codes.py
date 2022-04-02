b46321856b56fb297768a2516739250eecbbc50b
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-04-02 16:35:29 -0400

    add

--- Practice_codes.py
+++ Practice_codes.py
@@ -1,29 +1,29 @@
 # python local and global variables 
 
-# message="How are you doing?"
+message="How are you doing?"
 
-# def greet():
-#     message="How are you"
-#     print("Inside message", message)
+def greet():
+    message="How are you"
+    print("Inside message", message)
 
-# greet()
-# print("Outside message", message)
+greet()
+print("Outside message", message)
 
 # Global and Local variables
-# def add_numbers(n1,n2):
-#     result=n1+n2
-#     return result
-# output=add_numbers(2,5)
-# print(output)
+def add_numbers(n1,n2):
+    result=n1+n2
+    return result
+output=add_numbers(2,5)
+print(output)
 
 #Lists & Tuples in python - compound data types 
 
-# numbers= [1,5,6,-4]
-# print(numbers)
+numbers= [1,5,6,-4]
+print(numbers)
 
 
-# random_list=[2.5,"hello",-5,2.5]
-# print(random_list)
+random_list=[2.5,"hello",-5,2.5]
+print(random_list)
 
 languages=["Python","C++","Java",".Net"]
 #print(languages)
