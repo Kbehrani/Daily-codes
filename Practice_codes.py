1d79703878d266e73b7dd3d092409956965dfda0
Kbehrani <73279861+Kbehrani@users.noreply.github.com>
2022-04-02 06:14:24 -0400

    Add files via upload
    daily practice

---
+++ Practice_codes.py
@@ -0,0 +1,39 @@
+# python local and global variables 
+
+# message="How are you doing?"
+
+# def greet():
+#     message="How are you"
+#     print("Inside message", message)
+
+# greet()
+# print("Outside message", message)
+
+# Global and Local variables
+# def add_numbers(n1,n2):
+#     result=n1+n2
+#     return result
+# output=add_numbers(2,5)
+# print(output)
+
+#Lists & Tuples in python - compound data types 
+
+# numbers= [1,5,6,-4]
+# print(numbers)
+
+
+# random_list=[2.5,"hello",-5,2.5]
+# print(random_list)
+
+languages=["Python","C++","Java",".Net"]
+#print(languages)
+
+#index
+print(languages[1])
+#negative indexing
+print(languages[-3])
+
+print(languages[0:3]) #starting index is inclusive, last in exclusive
+print(languages[1:3])
+print(languages[:3]) #blank starting index is read as 0
+
