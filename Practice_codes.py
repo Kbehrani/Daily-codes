e86f9b789a6fdd36b189baeca12bc513b819ca10
Kavisha Behrani <kavishabehrani@Kavishas-MacBook-Air.local>
2022-04-02 06:43:17 -0400

    add

--- Practice_codes.py
+++ Practice_codes.py
@@ -37,5 +37,8 @@ print(languages[0:3]) #starting index is inclusive, last in exclusive
 print(languages[1:3])
 print(languages[:3]) #blank starting index is read as 0
 
-#learning new things daily
+#change items in a list
 
+#lists are mutable objects
+languages[1]="Ruby"
+print(languages)
