069ed081b03cef67a09ebdc3a6024b863acb83cf
Kavisha <Kavisha.behrani@gmal.com>
2022-04-10 16:34:11 -0400

    add

--- #17 Python dictionaries.py
+++ #17 Python dictionaries.py
@@ -2,20 +2,20 @@
 
 person1={"name":"linus", "age":21}
 print(person1)
-# person1={"name": "linus", "age": 21}
-# # print(person1["name"])
-# print(person1.get("hobbies"))
-# print(person1.get("hobbies", ["dancing", "fishing"]))
+person1={"name": "linus", "age": 21}
+# print(person1["name"])
+print(person1.get("hobbies"))
+print(person1.get("hobbies", ["dancing", "fishing"]))
 
-# person2={10:"linus", ("age"):21}
-# print(person2)
+person2={10:"linus", ("age"):21}
+print(person2)
 
 # add and change dictionary elemets
 person1={"name": "linus", "age": 21}
-# person1["name"]="dennis"
-# print(person1)
+person1["name"]="dennis"
+print(person1)
 person1["hobbies"]= ["dancing", "fishing"]
-# print(person1)
+print(person1)
 
 #removing an item from the dictionary
 # using pop method
@@ -24,4 +24,3 @@ person1["hobbies"]= ["dancing", "fishing"]
 for key in person1:
     print(key)
     print(person1[key])
-"finally"
