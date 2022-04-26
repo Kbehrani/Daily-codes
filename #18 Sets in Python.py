6f96c353d054c7a0995492324210713e1f963abc
Kavisha <Kavisha.behrani@gmal.com>
2022-04-26 05:45:56 -0400

    Add

--- #18 Sets in Python.py
+++ #18 Sets in Python.py
@@ -3,5 +3,15 @@
 #2 no duplicates are allowed
 #3 items must be immutable objects
 
-animals={"dog","cat","tiger","elephant"}
+animals={"dog","cat","tiger","elephant","dog"} #diuplicate value would be removed
+print(animals)
+
+animals=set()
+print(animals)
+
+animals=set(["cat","dog"]) #adding a list inside this set function
+print(animals)
+#sets are mutable - we can add/remmove from them
+animals={"dog","cat","tiger","elephant","dog"} #diuplicate value would be removed
+animals.add("monkey")
 print(animals)
