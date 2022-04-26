5bfa944ac9fd072d10bc3a5ba9d98c0bbe8dbcc5
Kavisha <Kavisha.behrani@gmal.com>
2022-04-26 05:49:40 -0400

    add

--- #18 Sets in Python.py
+++ #18 Sets in Python.py
@@ -12,6 +12,8 @@ print(animals)
 animals=set(["cat","dog"]) #adding a list inside this set function
 print(animals)
 #sets are mutable - we can add/remmove from them
-animals={"dog","cat","tiger","elephant","dog"} #diuplicate value would be removed
+animals={"dog","cat","tiger","elephant","dog"} #duplicate value would be removed
 animals.add("monkey")
 print(animals)
+
+learnt something new today
