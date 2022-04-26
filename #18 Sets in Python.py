3ec344afa80bef2640552977388a22199384d8f0
Kavisha <Kavisha.behrani@gmal.com>
2022-04-26 05:21:43 -0400

    add

---
+++ #18 Sets in Python.py
@@ -0,0 +1,7 @@
+#remember these things about sets
+#1 items of a set are in no particular order
+#2 no duplicates are allowed
+#3 items must be immutable objects
+
+animals={"dog","cat","tiger","elephant"}
+print(animals)
